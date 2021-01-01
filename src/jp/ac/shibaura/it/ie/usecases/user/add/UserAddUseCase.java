b06b37c1fcb64b41e7c75dc08f3a6912475eafb5
THEToilet <bp18015@shibaura-it.ac.jp>
2021-01-01 18:23:51 +0900

    add plantUML

--- src/jp/ac/shibaura/it/ie/usecases/user/add/UserAddUseCase.java
+++
@@ -1,5 +0,0 @@
-package jp.ac.shibaura.it.ie.usecases.user.add;
-
-import jp.ac.shibaura.it.ie.usecases.core.UseCase;
-public interface UserAddUseCase extends UseCase<UserAddInputData, UserAddOutputData> {
-}
